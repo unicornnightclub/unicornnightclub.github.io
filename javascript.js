185152d96a8eb0caab52a805605b6ecfe9b15a13
unicornnightclub <52003069+unicornnightclub@users.noreply.github.com>
2019-06-20 15:14:32 +0200

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
-
-  $(document).ready(function(){
-      $('.carousel').carousel();
-    });
